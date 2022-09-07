586146dbe930ee31f8bc046b833d609ee2533aa0
黄文浩 <huangwenhao23@163.com>
2022-09-07 15:15:00 +0800

    调整获取后内容修正

--- book.js
+++ book.js
@@ -1,74 +1,84 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
-const iconv = require('iconv-lite')
-const chalk = require('chalk')
-const request = require('request')
+const fs = require('fs');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const chalk = require('chalk');
+const request = require('request');
 
-const homePage = 'https://www.ranwen.tw/book_102363/'
+const homePage = 'https://www.yqzww.cc/book_115962/';
 
-let txtContent = []
-const bookName = '都市修仙1'
+let txtContent = [];
+const bookName = 'book';
 
 const fetchPage = (url) => {
-    return new Promise((resolve, reject) => {
-        request(
-            {
-                url,
-                encoding: null
-            },
-            (error, response, body) => {
-                if (error) reject(error);
-                if (response && response.statusCode === 200) {
-                    resolve(body);
-                } else {
-                    reject(`请求✿✿✿${url}✿✿✿失败`);
-                }
-            }
-        )
-    })
-}
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url,
+        encoding: null,
+      },
+      (error, response, body) => {
+        if (error) reject(error);
+        if (response && response.statusCode === 200) {
+          resolve(body);
+        } else {
+          reject(`请求✿✿✿${url}✿✿✿失败`);
+        }
+      }
+    );
+  });
+};
 
 const save = (content) => {
-    fs.writeFile(`${bookName}.txt`, content, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-    })
-}
+  fs.writeFile(`./dist/${bookName}.txt`, content, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+};
 
 const start = async (page, pageIndex) => {
-    const htmlBody = await fetchPage(page)
-    const data = iconv.decode(htmlBody, "gbk")
-    const $ = cheerio.load(data, { decodeEntities: false })
+  const htmlBody = await fetchPage(page);
+  const data = iconv.decode(htmlBody, 'gbk');
+  const $ = cheerio.load(data, { decodeEntities: false });
+  console.log('>\r\n', data);
+  const title = $('.bookname h1')
+    .text()
+    .replace(/^\s+|\s+$/g, '');
+  const content = $('#content').text().replace('言情中文网 www.yqzww.cc，最快更新女总裁的超级兵王 ！', '');
+  // let content = $('#content').text().replace('言情中文网 www.yqzww.cc，最快更新女总裁的超级兵王 ！', '').replace('  ', '\r\n');
 
-    const title = $('h1.h1title').text().replace(/^\s+|\s+$/g, '')
-    const content = $('#htmlContent').text()
-    
-    console.log(chalk.green(`> 内容解析成功 ${pageIndex}`) + '\n  ' + title + '\n  ' + page + '\n')
-    txtContent[pageIndex] = title + '\r\n' + content + '\r\n'
+  console.log(chalk.green(`> 内容解析成功 ${pageIndex}`) + '\n  ' + title + '\n  ' + page + '\n');
+  txtContent[pageIndex] = content + '\r\n';
 
-    const isCompleted = txtContent.every(v => v !== false)
+  const isCompleted = txtContent.every((v) => v !== false);
 
-    if (isCompleted) {
-        save(txtContent.join(''))
-    }
-}
+  if (isCompleted) {
+    save(txtContent.join(''));
+  }
+};
 
 const home = async (page) => {
-    const htmlBody = await fetchPage(page)
-    const data = iconv.decode(htmlBody, "gbk")
-    const $ = cheerio.load(data, { decodeEntities: false })
+  const htmlBody = await fetchPage(page);
+  const data = iconv.decode(htmlBody, 'gbk');
+  const $ = cheerio.load(data, { decodeEntities: false });
 
-    const wrap = $('.chapterlist').eq(1).find('li a')
-    txtContent = new Array(wrap.length).fill(false)
+  const wrap = $('#list').find('dt').eq(1).nextAll('dd');
+  txtContent = new Array(wrap.length).fill(false);
 
-    wrap.map((i, el) => {
-        const href = $(el).attr('href')
-        const pageUrl = `${homePage}${href}`
-        setTimeout(() => {
-            start(pageUrl, i)
-        }, i * 100)
-    })
-}
+  wrap.map((i, el) => {
+    const href = $(el).find('a').attr('href');
+    if (!href) {
+      txtContent[i] = '';
+      return;
+    }
+    // if (i > 1) {
+    //   return;
+    // }
+    const pageUrl = `${homePage}${href}`;
+    setTimeout(() => {
+      start(pageUrl, i);
+    }, i * 100);
+  });
+};
 
-home(homePage)
+home(homePage);
--- news.js
+++ news.js
@@ -4,64 +4,70 @@
  * @project: node cheerio 爬虫
  */
 
-const fs = require('fs')
-const cheerio = require('cheerio')
-const iconv = require('iconv-lite')
-const chalk = require('chalk')
-const request = require('request')
+const fs = require('fs');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const chalk = require('chalk');
+const request = require('request');
 
 // const baseUrl = 'https://www.bbiquge.cc'
 // let startPage = `${baseUrl}/book_115035/38582040.html`
-const startPage = 'https://www.ranwen.tw/book_102363/36262340.html'
+const startPage = 'https://www.ranwen.tw/book_102363/36262340.html';
 
-let pageIndex = 1, txtContent = ''
-const bookName = '都市修仙'
+let pageIndex = 1,
+  txtContent = '';
+const bookName = '都市修仙';
 
 class NEWS {
-    fetchPage(url) {
-        return new Promise((resolve) => {
-            request({
-                url,
-                encoding: null
-            }, (error, response, body) => {
-                if (error) reject(error);
-                if (response && response.statusCode === 200) {
-                    resolve(body);
-                } else {
-                    reject(`请求✿✿✿${url}✿✿✿失败`);
-                }
-            })
-        })
-    }
-
-    async start(page) {
-        const data = await this.fetchPage(page)
-        const html = iconv.decode(data, "gbk")
-        const $ = cheerio.load(html, { decodeEntities: false })
-        const title = $('h1.h1title').text().replace(/^\s+|\s+$/g, '')
-        const content = $('#htmlContent').text()
-        const nextUrl = $('#link-next').attr('href')
-
-        if (nextUrl) {
-            console.log(chalk.green(`> 内容解析成功 ${pageIndex}`) + '\n  ' + title + '\n  ' + nextUrl + '\n')
-            txtContent += title + '\r\n' + content
-            pageIndex += 1
-            this.start(nextUrl)
-        } else {
-            this.saveContent(txtContent)
-            console.log(chalk.red(`> 内容解析有误，即将重试…\n`))
-            this.start(page)
+  fetchPage(url) {
+    return new Promise((resolve) => {
+      request(
+        {
+          url,
+          encoding: null,
+        },
+        (error, response, body) => {
+          if (error) reject(error);
+          if (response && response.statusCode === 200) {
+            resolve(body);
+          } else {
+            reject(`请求✿✿✿${url}✿✿✿失败`);
+          }
         }
-    }
+      );
+    });
+  }
 
-    saveContent(content) {
-        fs.writeFile(`${bookName}.txt`, content, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-        })
+  async start(page) {
+    const data = await this.fetchPage(page);
+    const html = iconv.decode(data, 'gbk');
+    const $ = cheerio.load(html, { decodeEntities: false });
+    const title = $('h1.h1title')
+      .text()
+      .replace(/^\s+|\s+$/g, '');
+    const content = $('#htmlContent').text();
+    const nextUrl = $('#link-next').attr('href');
+
+    if (nextUrl) {
+      console.log(chalk.green(`> 内容解析成功 ${pageIndex}`) + '\n  ' + title + '\n  ' + nextUrl + '\n');
+      txtContent += title + '\r\n' + content;
+      pageIndex += 1;
+      this.start(nextUrl);
+    } else {
+      this.saveContent(txtContent);
+      console.log(chalk.red(`> 内容解析有误，即将重试…\n`));
+      this.start(page);
     }
+  }
+
+  saveContent(content) {
+    fs.writeFile(`${bookName}.txt`, content, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
 }
 
-const news = new NEWS()
-news.start(startPage)
+const news = new NEWS();
+news.start(startPage);
